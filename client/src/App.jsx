0e356d8e3b05845dacb93d6cde06ef8a02c37dd6
Soham Ghosh <s004soham@gmail.com>
2025-05-11 02:40:31 +0530

    Added edit post and delete post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import Post from "./Post";
 import { Route, Routes } from "react-router-dom";
 import { UserContextProvider } from "./UserContext.jsx";
 import CreatePost from "./pages/CreatePost.jsx";
+import PostPage from "./pages/PostPage.jsx";
+import EditPost from "./pages/EditPost.jsx";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/create" element={<CreatePost/>}/>
+          <Route path="/post/:id" element={<PostPage/>} />
+          <Route path="/edit/:id" element={<EditPost/>}/>
         </Route>
       </Routes>
     </UserContextProvider>
--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -1,17 +1,30 @@
-import {format} from 'date-fns'
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
 
-export default function Post({title,summary,cover,content,createdAt,author}){
-    return(
-        <div className="post">
-          <img src={'http://localhost:8000/'+cover} alt=''></img>
-          <div className="texts">
+export default function Post({
+  _id,
+  title,
+  summary,
+  cover,
+  content,
+  createdAt,
+  author,
+}) {
+  return (
+    <div className="post">
+      <Link to={`/post/${_id}`}>
+        <img src={"http://localhost:8000/" + cover} alt=""></img>
+      </Link>
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
           <h2>{title}</h2>
-          <p className="info">
-            <a className='author'>{author.username}</a>
-            <time>{format(new Date(createdAt),'d MMM yyyy HH:mm')}</time>
-          </p>
-          <p className='summary'>{summary}</p>
-          </div>
-          </div>
-    );
+        </Link>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{format(new Date(createdAt), "d MMM yyyy HH:mm")}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
