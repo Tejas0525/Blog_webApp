e87f0a0a81729c043b4e62a211657f625fd8ccf7
Tejas Patil <tp07474@gmail.com>
2025-05-11 21:54:38 +0530

    PUT method added for post

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const { request } = require("express");
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
@@ -8,16 +9,62 @@ const multer = require('multer');
 const fs = require('fs');
 const User = require('./models/User');
 const Post = require('./models/Post');
+const { error, info } = require("console");
 const uploadMiddleware = multer({dest:'uploads/'});
 
 const app = express();
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({origin:'http://localhost:3000',credentials:true,}));
 app.use(express.json());
+app.use(cookieParser());
 
 const salt = bcrypt.genSaltSync(10);//random string generation
 const secret = 'kd7ychrwhidqmr84r9';//random string generation
 
-mongoose.connect("mongodb+srv://cdac:2yNrWqmihgsrc0F5@blogcluster.jezgeaw.mongodb.net/?retryWrites=true&w=majority&appName=blogCluster");
+try{
+    mongoose.connect("mongodb+srv://cdac:2yNrWqmihgsrc0F5@blogcluster.jezgeaw.mongodb.net/?retryWrites=true&w=majority&appName=blogCluster");
+    console.log("BloggerDB Connected...!");
+}
+catch(error){
+    console.log("error in connecting bloggerDB")
+}
+
+
+app.get('/',(request,response)=>{
+    try{
+        response.json("Welcome to Blogger...");
+    }
+    catch(error){
+        response.status(500).json("Error in fetching",error);
+    }
+});
+
+app.get('/profile',(request,response)=>{
+    try{
+        const {token} = request.cookies;
+        jwt.verify(token,secret,{},(error,info)=>{
+        if(error){
+             throw error;
+        }
+        response.json(info);
+        });
+    }
+    catch(error){
+        response.json("Fetching error");
+    }
+});
+
+app.get('/post',async (request,response)=>{
+    try{
+        response.json(await Post.find());
+    }
+    catch(error){
+        response.status(500).send("Not available...!")
+    }
+});
+
+app.post('/logout', (request,response) => {
+  response.cookie('token', '').json("Logout sucsessfully");
+});
 
 app.post('/register', async (request, response) => {
     const { username, password } = request.body;
@@ -36,7 +83,7 @@ app.post('/register', async (request, response) => {
 app.post('/login', async (request,response)=>{
     const {username,password} = request.body;
     const userDoc = await User.find({username});
-    const passOk = bcrypt.compareSync(password,userDoc.password);
+    const passOk = bcrypt.compareSync(password,userDoc[0].password);
     if(passOk){
         jwt.sign({username,id:userDoc.id},secret,{},(error,token)=>{
             if(error)
@@ -49,15 +96,6 @@ app.post('/login', async (request,response)=>{
     }
 });
 
-app.get('/profile',(request,response)=>{
-    const {token} = request.cookies;
-    jwt.verify(token,secret,{},(error,info)=>{
-        if(error)
-            throw error;
-        response.json(info);
-    });
-});
-
 app.post('/post',uploadMiddleware.single('file'),async (request,response)=>{
     try{
     const{originalname} = request.file;
@@ -66,39 +104,61 @@ app.post('/post',uploadMiddleware.single('file'),async (request,response)=>{
     const newPath = path +'.'+ ext;
     fs.renameSync(path,newPath);
 
+     const {token} = req.cookies;
+      jwt.verify(token, secret, {}, async (err,info) => {
+    //if (err) throw err;
     const {title,summary,content} = request.body;
-    await postDoc = Post.create({
+    const postDoc = await Post.create({
         title,
         summary,
         content,
         cover:newPath,
-        author:
+        author:info.id
     });
-
     response.json(postDoc);
+    });
     }
     catch(error){
         response.status(500).json("Error in uploading...!",error);
     }
 });
 
-app.get('/post',async (request,response)=>{
-    try{
-        response.json(await Post.find());
+app.put('/post',uploadMiddleware.single('file'), async (request,response) => {
+  let newPath = null;
+  if (request.file) {
+    const {originalname,path} = request.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = request.cookies;
+  jwt.verify(token, secret, {}, async (error,info) => {
+    if (error) throw err;
+    const {id,title,summary,content} = request.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
     }
-    catch(error){
-        response.status(500).send("Not available...!")
-    }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    response.json(postDoc);
+  });
+
 });
 
 app.listen(7077, () => {
     try {
-        console.log('server is running on PORT:7077');
+        console.log('server is running on URL http://localhost:7077');
     }
     catch (error) {
         console.log("Error in Connection...!");
     }
 });
-
-//2yNrWqmihgsrc0F5
-//mongodb+srv://cdac:2yNrWqmihgsrc0F5@blogcluster.jezgeaw.mongodb.net/?retryWrites=true&w=majority&appName=blogCluster
